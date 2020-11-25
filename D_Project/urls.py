75c13b44287672cf4c95c332523c3b3b3a7d94cf
Sush2k21 <sushmi9191@gmail.com>
2020-11-25 19:16:16 +0530

    Edited

--- D_Project/urls.py
+++ D_Project/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# Hello Bro
+from django.contrib import admin 
 from django.contrib.auth import views as auth_views
 from django.urls import path,include
 from django.conf import settings
